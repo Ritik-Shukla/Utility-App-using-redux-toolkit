f7327ce5af009ce9cd830ae054a5660c4839fccb
Ritik <shuklaritik731@gmail.com>
2023-11-03 01:16:33 +0530

    files added

---
+++ src/components/NoteList/NoteList.js
@@ -0,0 +1,28 @@
+
+import { useSelector, useDispatch } from "react-redux";
+// import { deleteNote } from "../../redux/actions/noteActions";
+import { actions } from "../../redux/reducers/noteReducer";
+import styles from "./NoteList.module.css";
+import { todoSelector } from "../../redux/reducers/todoReducer";
+
+function NoteList() {
+    const notes=useSelector(todoSelector);
+    const dispatch=useDispatch();
+
+  return (
+    <div className="container">
+    <ul>
+      {notes.map((note,index) => (
+        <li className={styles.item}>
+            <p>{note.createdOn.toLocaleDateString()}</p>
+            <p className={styles.notecontent}>{note.text}</p>
+            <button className={"btn btn-danger "+styles.button}
+            onClick={()=> dispatch(actions.delete(index))}>Delete</button>
+            </li>
+      ))}
+    </ul>
+    </div>
+  );
+}
+
+export default NoteList;
